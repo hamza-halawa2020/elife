aefcc6eeb3ef8781a6b0f67cd159eeca560ed9e4
hamza halawa <69251476+hamza-halawa2020@users.noreply.github.com>
2024-01-31 17:04:48 +0200

    dsfgsdfds

--- front/src/environments/environment.development.ts
+++ front/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     // backEndUrl: 'http://127.0.0.1:8000/api/',
     backEndUrl:'https://back.elifeegypt.com/public/index.php/api/',
-    // imgUrl:'http://127.0.0.1:8000/',
-    imgUrl:'https://back.elifeegypt.com/public/images/',
+    imgUrl:'http://127.0.0.1:8000/',
+    // imgUrl:'https://back.elifeegypt.com/public/images/',
 };
 
 
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    // production: true,
     // backEndUrl: 'http://127.0.0.1:8000/api/',
     backEndUrl:'https://back.elifeegypt.com/public/index.php/api/',
     // imgUrl:'http://127.0.0.1:8000/',
