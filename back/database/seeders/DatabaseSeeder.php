800eb28d8726882927ee0a3c213993845e7c27db
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-08-09 23:54:12 +0300

    up

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class DatabaseSeeder extends Seeder
@@ -18,7 +19,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'hamza@hamza.com',
         ], [
             'name' => 'hamza',
-            'password' => '123456',
+            'password' => Hash::make('123456'),
         ]);
     }
 }
