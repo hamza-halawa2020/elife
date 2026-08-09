f69f849051f1db6ff3510d44fb501cdb9b2f84b5
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-08-09 23:49:58 +0300

    up

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use \App\Models\User;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        User::factory()->create([
-            'name' => 'hamza',
+        User::query()->updateOrCreate([
             'email' => 'hamza@hamza.com',
-            'password'=> bcrypt('123456'),
-
+        ], [
+            'name' => 'hamza',
+            'password' => '123456',
         ]);
     }
 }
