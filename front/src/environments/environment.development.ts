80a408180a20b33557c3e1c5b15f848cf277cf08
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-08-09 23:45:43 +0300

    up

--- front/src/environments/environment.development.ts
+++ front/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // backEndUrl: 'http://127.0.0.1:8000/api/',
-  backEndUrl: 'https://back.elifeegypt.com/public/index.php/api/',
+  backEndUrl: 'https://back.elifeegypt.com/api/',
   // imgUrl:'http://127.0.0.1:8000/',
-  imgUrl: 'https://back.elifeegypt.com/publicimages/',
+  imgUrl: 'https://back.elifeegypt.com/images/',
 };
